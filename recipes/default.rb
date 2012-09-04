f88d66edec16f69e61f184cabb561d1f0bfe2c0f
Steffen Gebert <steffen.gebert@typo3.org>
2012-09-05 00:21:29 +0200

    Move cron job file to scripts directory

--- recipes/default.rb
+++ recipes/default.rb
@@ -203,22 +203,22 @@ end
 # Cron-Job
 ####################################
 
-directory "#{node['gerrit']['home']}/cron" do
+directory "#{node['gerrit']['home']}/scripts" do
   owner node['gerrit']['user']
 end
 
-template "#{node['gerrit']['home']}/cron/repack-repositories.sh" do
+template "#{node['gerrit']['home']}/scripts/repack-repositories.sh" do
   source "scripts/repack-repositories.sh"
   owner node['gerrit']['user']
   group node['gerrit']['group']
   mode 0744
 end
 
-cron "repack repositories" do
+cron "repack-repositories" do
   hour "2"
   minute "0"
   weekday "0"
-  command "#{node['gerrit']['home']}/cron/repack-repositories.sh"
+  command "#{node['gerrit']['home']}/scripts/repack-repositories.sh"
   user node['gerrit']['user']
 end
 
