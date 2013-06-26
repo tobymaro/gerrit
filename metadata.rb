4f9c5fd02aa03a7be1dfe785929bcbacbff93126
Steffen Gebert <steffen.gebert@typo3.org>
2013-06-26 21:49:10 +0200

    Bump version to 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures gerrit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.5"
+version          "0.3.0"
 
 %w{ build-essential database mysql java git maven apache2 ssl_certificates}.each do |cookbook|
   depends cookbook
