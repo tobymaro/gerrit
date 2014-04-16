f3d4bd2ae03f65ef787d43d6e4c36abc697291f2
Steffen Gebert <steffen.gebert@typo3.org>
2014-04-16 17:49:32 +0200

    add helper library

---
+++ libraries/helpers.rb
@@ -0,0 +1,40 @@
+#
+# Cookbook Name:: gerrit
+# Library:: helpers
+#
+# Copyright 2014, TYPO3 Association
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+module Gerrit
+  module Helpers
+
+    # Checks against the current gerrit version
+    #
+    # @return [Boolean]
+    def gerrit_above?(version)
+      require 'chef/version_constraint'
+
+      # remove the garbage (like -rc1 etc.) from version numbers
+      actual_version_cleaned = node['gerrit']['version'].split('-')[0]
+      Chef::VersionConstraint.new(">= #{version}").include?(actual_version_cleaned)
+    end
+  end
+end
+
+Chef::Node::Attribute.send(:include, ::Gerrit::Helpers)
+Chef::Recipe.send(:include, ::Gerrit::Helpers)
+Chef::Resource.send(:include, ::Gerrit::Helpers)
+Chef::Provider.send(:include, ::Gerrit::Helpers)
