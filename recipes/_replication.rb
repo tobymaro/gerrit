9f38e4d33b678c8b47d24c8dabd527c75ea88eb7
Steffen Gebert <steffen.gebert@typo3.org>
2014-05-25 15:37:38 +0200

    Replication JAR file is installed from gerrit.war

--- recipes/_replication.rb
+++ recipes/_replication.rb
@@ -1,15 +1,6 @@
-if node['gerrit'].attribute?('replication')
-  remote_file "#{node['gerrit']['home']}/review/plugins/replication.jar" do
-    owner node['gerrit']['user']
-    source node['gerrit']['replication']['plugin_download_url']
-    action :create_if_missing
-  end
-
-  template "#{node['gerrit']['install_dir']}/etc/replication.config" do
-    source "gerrit/replication.config"
-    owner node['gerrit']['user']
-    group node['gerrit']['group']
-    mode 0644
-    notifies :restart, "service[gerrit]"
-  end
+template "#{node['gerrit']['install_dir']}/etc/replication.config" do
+  source "gerrit/replication.config"
+  owner node['gerrit']['user']
+  group node['gerrit']['group']
+  mode 0644
 end
