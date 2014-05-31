bed53d30659bc7bc78c10afbd26e5f1495c28437
Steffen Gebert <steffen.gebert@typo3.org>
2014-05-31 02:14:36 +0200

    Compare database.type in upcase

--- recipes/_database.rb
+++ recipes/_database.rb
@@ -1,5 +1,5 @@
-if node['gerrit']['config']['database']['type'] == "MYSQL"
+if node['gerrit']['config']['database']['type'].upcase == "MYSQL"
   include_recipe "gerrit::mysql"
-elsif node['gerrit']['config']['database']['type'] == "POSTGRESQL"
+elsif node['gerrit']['config']['database']['type'].upcase == "POSTGRESQL"
   include_recipe "gerrit::postgresql"
 end
