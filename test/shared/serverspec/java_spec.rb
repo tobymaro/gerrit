7324dd609dfcb436010b5b752c49556edbf5cede
Steffen Gebert <steffen.gebert@typo3.org>
2014-05-31 02:27:36 +0200

    Enable test for java 7

--- test/shared/serverspec/java_spec.rb
+++ test/shared/serverspec/java_spec.rb
@@ -7,11 +7,6 @@ include Serverspec::Helper::DetectOS
 
 
 describe 'java' do
-
-  # Disable this test for the moment
-  before { pending "until the cookbook is Java 7 only" }
-
-
   it 'installs OpenJDK Java 7' do
     expect(command 'java -version').to return_stdout /java version "1.7.*/
   end
