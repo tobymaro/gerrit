51b05fb86f3f81c85a2d8c1ede2bcdf388063a26
Peter Niederlag <peter.niederlag@datenbetrieb.de>
2014-06-12 15:00:07 +0200

    set default Rspec params for platform to debian 7.4

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,16 @@ require_relative 'helpers/matchers'
 # Require all our libraries
 Dir['libraries/*.rb'].each { |f| require File.expand_path(f) }
 
+RSpec.configure do |config|
+
+  # Specify the Chef log_level (default: :warn)
+  config.log_level = :warn
+
+  # Specify the operating platform to mock Ohai data from (default: nil)
+  config.platform = 'debian'
+
+  # Specify the operating version to mock Ohai data from (default: nil)
+  config.version = '7.4'
+end
+
 #at_exit { ChefSpec::Coverage.report! }
