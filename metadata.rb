6503b3414bd4376818d27f1ba49cb9686fc38a35
Peter Niederlag <netservice@niekom.de>
2014-07-02 21:45:43 +0200

    version 0.3.2, compat with mysql cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures gerrit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.1"
+version          "0.3.2"
 
 %w{ build-essential database mysql postgresql java git maven apache2 cron }.each do |cookbook|
   depends cookbook
