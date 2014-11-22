248b09068d93bc854001a3e13fe9939174f0e915
Steffen Gebert <steffen.gebert@typo3.org>
2014-11-22 22:11:48 +0100

    Lock dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,10 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures gerrit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.4"
+version          "0.4.5"
 
-%w{ database mysql postgresql java git apache2}.each do |cookbook|
-  depends cookbook
-end
-
-recipe "gerrit::default", "Installs and configures Gerrit. Includes other recipes, if needed"
-recipe "gerrit::mysql", "Installs MySQL server and configures Gerrit to use MySQL"
-recipe "gerrit::proxy", "Installs Apache2 as reverse proxy in front of Gerrit"
-
-supports "debian"
+depends "apache2", "= 2.0.0"
+depends "database", "= 1.3.12"
+depends "mysql", "= 1.3.0"
+depends "java", "= 1.11.4"
+depends "git", "= 0.9.0"
