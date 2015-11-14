86a964171cd26505279e3e8194891acb5c0ae098
Steffen Gebert <steffen.gebert@typo3.org>
2015-11-14 11:42:04 +0100

    Bump version to 0.4.10

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures gerrit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.9"
+version          "0.4.10"
 
 depends "apache2", "~> 3.1.0"
 depends "database", "= 1.3.12"
