a39cd132d2d915992b985b238c1de60360eeb74a
Steffen Gebert <steffen.gebert@typo3.org>
2016-10-02 16:04:40 +0200

    Bump version to 0.6.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures gerrit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.6.0"
+version          "0.6.1"
 
 depends "apache2"
 depends "build-essential"
